42eba82c1204f3c6ecadc1716e569bd3251042af
lamborghini61 <296700995@qq.com>
2014-09-13 22:59:17 +0800

    hi

--- link.c
+++ link.c
@@ -1,7 +1,7 @@
 /**************************************************************************************************/
 /* Copyright (C) Sun Zenan, SSE@USTC, 2014-2015                                                   */
 /*                                                                                                */
-/*  FILE NAME             :  ftpmenu.c                                                            */
+/*  FILE NAME             :  link.c                                                               */
 /*  PRINCIPAL AUTHOR      :  SunZenan                                                             */
 /*  SUBSYSTEM NAME        :  ftpmenu                                                              */
 /*  MODULE NAME           :  ftpmenu                                                              */
@@ -20,11 +20,18 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-#include<list.h>
+#include"link.h"
+static tDataNode menulist[]=
+{     
+    {"showlist","it is a showlist cmd\n",showlist,&menulist[1]},
+    {"version","it is the first version\n",NULL,&menulist[2]},
+    {"send","it is a send cmd\n",NULL,&menulist[3]},
+    {"rec","it is a rec cmd\n",NULL,NULL}
+};
 
 int showlist()
 {
-    tDataNode *p=&menulist[1];
+    tDataNode *p=menulist;
     while(p)
     {
         printf("%s,%s",p->cmd,p->dsc);
--- link.h
+++ link.h
@@ -1,7 +1,7 @@
 /**************************************************************************************************/
 /* Copyright (C) Sun Zenan, SSE@USTC, 2014-2015                                                   */
 /*                                                                                                */
-/*  FILE NAME             :  ftpmenu.c                                                            */
+/*  FILE NAME             :  link.h                                                               */
 /*  PRINCIPAL AUTHOR      :  SunZenan                                                             */
 /*  SUBSYSTEM NAME        :  ftpmenu                                                              */
 /*  MODULE NAME           :  ftpmenu                                                              */
@@ -18,35 +18,15 @@
  *
  */
 
-#include<stdio.h>
-#include<stdlib.h>
-#include"link.h"
 
-#define DESC_LEN         1024
-#define CMD_MAX_LENS     50
-
-static tDataNode menulist[]=
+typedef struct DataNode
 {
-    {"showlist","it is a showlist cmd\n",showlist,&menulist[1]},
-    {"version","it is the first version\n",NULL,&menulist[2]},
-    {"send","it is a send cmd\n",NULL,&menulist[3]},
-    {"rec","it is a rec cmd\n",NULL,NULL}
-};
+    char*   cmd;
+    char*   dsc;
+    int     (*handler)();
+    struct  DataNode *next;
+} tDataNode;
 
-main()
-{   while(1)
-    {char cmd[CMD_MAX_LENS]; 
-     printf("please enter the cmd:\n");
-     scanf("%s",cmd);
-     tDataNode *P;
-     tDataNode* p=FindCmd(menulist,cmd);
-     if(p!=NULL)
-    {
-       printf("%s- %s\n",p->cmd,p->dsc);
-       if(p->handler!=NULL)
-           p->handler();
-    }
-    else printf("it is a wrong number\n");
-    } 
-}
+int showlist();
 
+tDataNode* FindCmd(tDataNode *head,char *cmd);
