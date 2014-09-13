99a30e62516891ca5dbadb5078c05f6ba2667eed
lamborghini61 <296700995@qq.com>
2014-09-13 15:27:07 +0800

    yes

--- ftpmenu.c
+++ ftpmenu.c
@@ -19,21 +19,24 @@
 #define DESC_LEN         1024
 #define CMD_MAX_LENS     50
 
+int showlist();
+
 typedef struct DataNode
 {
-    char   *cmd;
-    char   *desc;
-    struct DataNode *next;
+    char*   cmd;
+    char*   dsc;
+    int     (*handler)();
+    struct  DataNode *next;
 } tDataNode;
 
 
 static tDataNode menulist[]=
 {
 
-    {"help","it is a help cmd",&menulist[1]},
-    {"version","it is the first version",&menulist[2]},
-    {"send","it is a send cmd",&menulist[3]},
-    {"rec","it is a rec cmd",NULL}
+    {"showlist","it is a showlist cmd\n",showlist,&menulist[1]},
+    {"version","it is the first version\n",NULL,&menulist[2]},
+    {"send","it is a send cmd\n",NULL,&menulist[3]},
+    {"rec","it is a rec cmd\n",NULL,NULL}
 };
 
 
@@ -50,8 +53,10 @@ main()
         {   
             if(!strcmp(p->cmd,cmd))
             {
-	        printf("%s- %s\n",p->cmd,p->desc);
-	        break;
+	        printf("%s- %s\n",p->cmd,p->dsc);
+	        if(p->handler!=NULL)
+                p->handler();
+                break;
             } 
 
             p=p->next;
@@ -61,4 +66,47 @@ main()
             printf("it is a wrong cmd\n");
         }
      }
-} 
+}
+
+int showlist()
+{
+    tDataNode *p=&menulist[1];
+    while(p)
+    {
+        printf("%s,%s",p->cmd,p->dsc);
+        p=p->next;
+    }
+    return 0;
+}
+
+tDataNode *FindCmd(tDataNode *head)
+{  tDataNode *p=head;  
+ while(1)
+    {
+        char cmd[CMD_MAX_LENS];
+        tDataNode *head=NULL;
+        printf("please enter cmd:\n");
+        scanf("%s",&cmd);
+        while(p!=NULL)
+        {
+            if(!strcmp(p->cmd,cmd))
+            {
+                printf("%s- %s\n",p->cmd,p->dsc);
+                if(p->handler!=NULL)
+                p->handler();
+                return p;
+            }
+
+            p=p->next;
+        }
+        if(p==NULL)
+        {
+            printf("it is a wrong cmd\n");
+         return NULL;
+        }
+     }
+
+
+
+
+}
